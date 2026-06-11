fd0ab5fcb94b3d2b16b3f187b5d5f36c8c5e6009
Err <nmtnguyen56@gmail.com>
2026-06-11 14:16:59 +0700

    Update uninstall.php file.

--- uninstall.php
+++ uninstall.php
@@ -10,39 +10,45 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 /**
+ * Delete per-user settings notice transients for the current site.
+ */
+function chout_aio_delete_settings_updated_transients() {
+	$chout_aio_user_ids = get_users(
+		array(
+			'fields' => 'ID',
+		)
+	);
+
+	foreach ( $chout_aio_user_ids as $chout_aio_user_id ) {
+		delete_transient( 'chout_aio_settings_updated_' . $chout_aio_user_id );
+	}
+}
+
+/**
  * Delete plugin data for the current site.
  */
 function chout_aio_uninstall_site() {
 	delete_option( 'chout_aio_features' );
 	delete_option( 'chout_aio_scroll_add_action_class' );
-
-	global $wpdb;
-
-	$transient_like = $wpdb->esc_like( '_transient_chout_aio_settings_updated_' ) . '%';
-	$timeout_like   = $wpdb->esc_like( '_transient_timeout_chout_aio_settings_updated_' ) . '%';
-
-	$wpdb->query(
-		$wpdb->prepare(
-			"DELETE FROM {$wpdb->options} WHERE option_name LIKE %s OR option_name LIKE %s",
-			$transient_like,
-			$timeout_like
-		)
-	);
+	chout_aio_delete_settings_updated_transients();
 }
 
 if ( is_multisite() ) {
-	$site_ids = get_sites(
+	$chout_aio_site_ids = get_sites(
 		array(
 			'fields' => 'ids',
 			'number' => 0,
 		)
 	);
 
-	foreach ( $site_ids as $site_id ) {
-		switch_to_blog( $site_id );
+	foreach ( $chout_aio_site_ids as $chout_aio_site_id ) {
+		switch_to_blog( $chout_aio_site_id );
 		chout_aio_uninstall_site();
 		restore_current_blog();
 	}
 } else {
 	chout_aio_uninstall_site();
 }
+
+delete_site_option( 'external_updates-chout-all-in-one' );
+delete_site_transient( 'puc_manual_check_errors-chout-all-in-one' );
